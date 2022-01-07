b28c88af32790debb6069242d3da1ea3e96b0811
Aji Furqon <furqonajiy@caditglobal.com>
2022-01-07 11:03:02 +0700

    Create URLCheckController.java

---
+++ is-the-site-up/src/main/java/io/furqonajiy/isthesiteup/controllers/URLCheckController.java
@@ -0,0 +1,42 @@
+package io.furqonajiy.isthesiteup.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class URLCheckController {
+    private final String SITE_IS_UP = "Site is Up!";
+    private final String SITE_IS_DOWN = "Site is Down!";
+    private final String INCORRECT_URL = "URL is Incorrect!";
+
+    @GetMapping("/check")
+    public String getURLStatusMessage(@RequestParam String url) {
+        String returnMessage = "";
+        
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCodeCategory = conn.getResponseCode() / 100;
+            if(responseCodeCategory == 2 || responseCodeCategory == 3) {
+                returnMessage = SITE_IS_UP;
+            } else {
+                returnMessage = SITE_IS_DOWN;
+            }
+        } catch (MalformedURLException e) {
+            returnMessage = INCORRECT_URL;
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+
+        return returnMessage;
+    }
+}
